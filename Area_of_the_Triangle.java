8056cb00c840e8d0f9de8f1a851b13939a9793ba
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-09-20 22:01:14 +0530

    import java.util.Scanner; import java.io.*; import java.lang.*; class area{     public static void main(String [] args){         Scanner s=new Scanner(System.in);         float x=s.nextFloat();         float y=s.nextFloat();         float z=s.nextFloat();         float j=(x+y+z)/2;         double key=Math.sqrt(j*(j-x)*(j-y)*(j-z));         System.out.printf("%.2f",key);     }
    }

---
+++ Area_of_the_Triangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.*;
+import java.lang.*;
+class area{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        float x=s.nextFloat();
+        float y=s.nextFloat();
+        float z=s.nextFloat();
+        float j=(x+y+z)/2;
+        double key=Math.sqrt(j*(j-x)*(j-y)*(j-z));
+        System.out.printf("%.2f",key);
+    }
+    
+}
