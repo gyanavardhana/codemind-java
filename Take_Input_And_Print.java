32ff90dc36c0eb95eab8c1a6b80d7b7a17cd8dfa
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-09-27 00:15:56 +0530

    import java.util.Scanner; import java.io.*; class n{     public static void main(String [] args){         int t;         Scanner s=new Scanner(System.in);         String sc=s.nextLine();         System.out.println(sc);
            }
        }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.*;
+class n{
+    public static void main(String [] args){
+        int t;
+        Scanner s=new Scanner(System.in);
+        String sc=s.nextLine();
+        System.out.println(sc);
+            
+        }
+    }
