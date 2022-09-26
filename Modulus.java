8b9d35c6cc6df43e4c886aa653faa066a8c887a3
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-09-27 00:09:06 +0530

    import java.util.Scanner; import java.io.*; class n{     public static void main(String [] args){         int t;         Scanner s=new Scanner(System.in);         t=s.nextInt();         int j=s.nextInt();         System.out.println(t%j);
            }
        }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.*;
+class n{
+    public static void main(String [] args){
+        int t;
+        Scanner s=new Scanner(System.in);
+        t=s.nextInt();
+        int j=s.nextInt();
+        System.out.println(t%j);
+            
+        }
+    }
