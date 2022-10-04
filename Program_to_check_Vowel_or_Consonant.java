6886b08ace477e0200daf5b08a1727ed8216128b
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2022-10-04 17:07:32 +0530

    import java.util.Scanner; class VowelConsonant {
        public static void main(String[] args) {
            Scanner s=new Scanner(System.in);
    
            char ch =s.next().charAt(0);
    
            switch (ch) {
                case 'a':
                case 'e':
                case 'i':
                case 'o':
                case 'u':
                case 'A':
                case 'E':
                case 'I':
                case 'O':
                case 'U':
                    System.out.println("Vowel");
                    break;
                default:
                    System.out.println("Consonant");
            }
        }
    }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class VowelConsonant {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+
+        char ch =s.next().charAt(0);
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
+    }
+}
