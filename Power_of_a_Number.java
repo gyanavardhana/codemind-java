aefe032b4dfa3729f9ae7de4a8a8f8d88be20d88
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-02-05 16:23:44 +0530

    import java.util.*; class J {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         System.out.println((int)(Math.pow(a,b))%c);     } }

---
+++ Power_of_a_Number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class J
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.println((int)(Math.pow(a,b))%c);
+    }
+}
