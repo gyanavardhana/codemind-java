6ddea017f3704f530d9268c655e0dc7e25bedd04
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-02-05 16:50:36 +0530

    import java.util.Scanner; class divisible {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,a,c=0;         n=sc.nextInt();         for(i=0;i<n;i++)         {             a=sc.nextInt();             if(a%2!=0)             {                 if(a!=0)                 {                    c=i;                 }             }         }         System.out.print(c);         sc.close();     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class divisible
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,a,c=0;
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            a=sc.nextInt();
+            if(a%2!=0)
+            {
+                if(a!=0)
+                {
+                   c=i;
+                }
+            }
+        }
+        System.out.print(c);
+        sc.close();
+    }
+}
