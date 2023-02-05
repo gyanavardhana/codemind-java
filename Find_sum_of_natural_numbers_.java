551ca7bb69e5dd89454437a13d423d566d0d8674
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-02-05 17:24:01 +0530

    import java.util.*; class j{     public static void main(String[]args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int s = 0;         for(int i=0; i<=n; i++){             s+=i;         }         System.out.println(s);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class j{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = 0;
+        for(int i=0; i<=n; i++){
+            s+=i;
+        }
+        System.out.println(s);
+    }
+}
