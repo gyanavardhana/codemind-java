51a0a8b479f91600e8c61b9246f65b48b32e686d
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-02-05 16:47:17 +0530

    import java.util.*; class j {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int e=0,o=0;         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int c=0;         int k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(a[i]==k)             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ count_of_given_element_in_an_array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class j
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int e=0,o=0;
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int c=0;
+        int k=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==k)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
