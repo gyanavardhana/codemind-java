90c97f820f5ed9dc1a43501a4bf64130efba01d1
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-02-05 16:52:47 +0530

    import java.util.Scanner; class even {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0;         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=1;i<n-1;i++)     {         if((arr[i-1]%2==1 && arr[i+1]%2==1) && arr[i]%2==0)         {             c++;         }     }     System.out.print(c);     } }

---
+++ count_even_numbers_between_odd_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class even
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=1;i<n-1;i++)
+    {
+        if((arr[i-1]%2==1 && arr[i+1]%2==1) && arr[i]%2==0)
+        {
+            c++;
+        }
+    }
+    System.out.print(c);
+    }
+}
