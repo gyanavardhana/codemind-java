b8b4b2684b8df5c3248e06326aa2cb5ba19f0813
gyanavardhana <89439095+gyanavardhana@users.noreply.github.com>
2023-03-18 17:11:31 +0530

    Update Buy_Books.java

--- Buy_Books.java
+++ Buy_Books.java
@@ -6,6 +6,8 @@ class J
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        
+        
         int a[]=new int[n];
         int b[]=new int[n];
         for(int i=0;i<n;i++)
